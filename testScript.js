e184e2e28ec516c2c75e7dd205994ca7cb6e61af
Richard Blundell <rblundell@deloitte.com>
2019-05-03 15:39:24 -0400

    initial checkin

--- testScript.js
+++ testScript.js
@@ -1,134 +1,137 @@
 #!/usr/bin/env node
+
 process.title = 'Richard';
 
-var blessed = require('blessed')
-  , screen;
+var blessed = require('blessed'),
+    screen;
 
 screen = blessed.screen({
-  smartCSR: true,
-  log: process.env.HOME + '/blessed-terminal.log',
-  fullUnicode: true,
-  dockBorders: true,
-  ignoreDockContrast: true
+    smartCSR: true,
+    log: process.env.HOME + '/blessed-terminal.log',
+    fullUnicode: true,
+    dockBorders: true,
+    ignoreDockContrast: true
 });
 
 var topleft = blessed.terminal({
-  parent: screen,
-  cursor: 'line',
-  cursorBlink: true,
-  screenKeys: false,
-  label: ' multiplex.js ',
-  left: 0,
-  top: 0,
-  width: '50%',
-  height: '50%',
-  border: 'line',
-  style: {
-    fg: 'default',
-    bg: 'default',
-    focus: {
-      border: {
-        fg: 'green'
-      }
+    parent: screen,
+    shell: 'ssh',
+    args: ['-t', 'cis-dev'],
+    cursor: 'block',
+    cursorBlink: true,
+    screenKeys: false,
+    label: ' multiplex.js ',
+    left: 0,
+    top: 0,
+    width: '50%',
+    height: '50%',
+    border: 'line',
+    style: {
+        fg: 'default',
+        bg: 'default',
+        focus: {
+            border: {
+                fg: 'green'
+            }
+        }
     }
-  }
 });
 
 topleft.pty.on('data', function(data) {
-  screen.log(JSON.stringify(data));
+    screen.log(JSON.stringify(data));
 });
 
 var topright = blessed.terminal({
-  parent: screen,
-  cursor: 'block',
-  cursorBlink: true,
-  screenKeys: false,
-  label: ' multiplex.js ',
-  left: '50%-1',
-  top: 0,
-  width: '50%+1',
-  height: '50%',
-  border: 'line',
-  style: {
-    fg: 'red',
-    bg: 'black',
-    focus: {
-      border: {
-        fg: 'green'
-      }
+    parent: screen,
+    cursor: 'block',
+    cursorBlink: true,
+    screenKeys: false,
+    label: ' multiplex.js ',
+    left: '50%-1',
+    top: 0,
+    width: '50%+1',
+    height: '50%',
+    border: 'line',
+    style: {
+        fg: 'default',
+        bg: 'default',
+        focus: {
+            border: {
+                fg: 'green'
+            }
+        }
     }
-  }
 });
 
 var bottomleft = blessed.terminal({
-  parent: screen,
-  cursor: 'block',
-  cursorBlink: true,
-  screenKeys: false,
-  label: ' multiplex.js ',
-  left: 0,
-  top: '50%-1',
-  width: '50%',
-  height: '50%+1',
-  border: 'line',
-  style: {
-    fg: 'default',
-    bg: 'default',
-    focus: {
-      border: {
-        fg: 'green'
-      }
+    parent: screen,
+    cursor: 'block',
+    cursorBlink: true,
+    screenKeys: false,
+    label: ' multiplex.js ',
+    left: 0,
+    top: '50%-1',
+    width: '50%',
+    height: '50%+1',
+    border: 'line',
+    style: {
+        fg: 'default',
+        bg: 'default',
+        focus: {
+            border: {
+                fg: 'green'
+            }
+        }
     }
-  }
 });
 
 var bottomright = blessed.terminal({
-  parent: screen,
-  cursor: 'block',
-  cursorBlink: true,
-  screenKeys: false,
-  label: ' multiplex.js ',
-  left: '50%-1',
-  top: '50%-1',
-  width: '50%+1',
-  height: '50%+1',
-  border: 'line',
-  style: {
-    fg: 'default',
-    bg: 'default',
-    focus: {
-      border: {
-        fg: 'green'
-      }
+    parent: screen,
+    cursor: 'block',
+    cursorBlink: true,
+    screenKeys: false,
+    label: ' multiplex.js ',
+    left: '50%-1',
+    top: '50%-1',
+    width: '50%+1',
+    height: '50%+1',
+    border: 'line',
+    style: {
+        fg: 'default',
+        bg: 'default',
+        focus: {
+            border: {
+                fg: 'green'
+            }
+        }
     }
-  }
 });
 
 [topleft, topright, bottomleft, bottomright].forEach(function(term) {
-  term.enableDrag(function(mouse) {
-    return !!mouse.ctrl;
-  });
-  term.on('title', function(title) {
-    screen.title = title;
-    term.setLabel(' ' + title + ' ');
-    screen.render();
-  });
-  term.on('click', term.focus.bind(term));
+    term.enableDrag(function(mouse) {
+        return !!mouse.ctrl;
+    });
+    term.on('title', function(title) {
+        screen.title = title;
+        term.setLabel(' ' + title + ' ');
+        screen.render();
+    });
+    term.on('click', term.focus.bind(term));
 });
 
 topleft.focus();
 
 screen.key('C-q', function() {
-  topleft.kill();
-  topright.kill();
-  bottomleft.kill();
-  bottomright.kill();
-  return screen.destroy();
+    topleft.kill();
+    topright.kill();
+    bottomleft.kill();
+    bottomright.kill();
+    return screen.destroy();
 });
 
 screen.program.key('S-tab', function() {
-  screen.focusNext();
-  screen.render();
+    screen.focusNext();
+    screen.render();
 });
 
 screen.render();
